f5bd8d5a7d863d0b9b324c4d28e40515b44aa89c
Panaria <baranyldzby@gmail.com>
2023-10-02 21:21:57 +0300

    Add project files.

---
+++ ortalama hesaplama/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ortalama_hesaplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Matematik dersinden 1. sınavı = 55, 2. sınavı = 65, üçüncü sınavı = 88 olan bir öğrencinin ortalamasını hesaplayınız.
+
+            Console.WriteLine(("Ortalama = ")+(55 + 65 + 88) / 3);
+            
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
